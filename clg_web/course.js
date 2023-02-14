7c54bfe828aebb627d7ca31d3d5f3df38e6eb315
Mari0087 <105765015+Mari0087@users.noreply.github.com>
2023-02-14 20:43:05 +0530

    Add files via upload

---
+++ clg_web/course.js
@@ -0,0 +1,15 @@
+function ugOn() {
+    document.getElementById("ug-overlay").style.display = "block";
+  }
+  
+function ugOff() {
+    document.getElementById("ug-overlay").style.display = "none";
+  }
+
+function pgOn() {
+    document.getElementById("pg-overlay").style.display = "block";
+  }
+  
+function pgOff() {
+    document.getElementById("pg-overlay").style.display = "none";
+  }
